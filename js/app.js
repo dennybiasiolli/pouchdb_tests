62af46039f5cb4016cdb4958ece338830b29781e
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-08-25 11:20:36 +0200

    enable sample editing of todos

--- js/app.js
+++ js/app.js
@@ -4,14 +4,19 @@ app.controller('AppController', function($scope, $q) {
   var db = new PouchDB('todos');
 
   $scope.addTodo = function() {
-    var todo = {
+    $scope.writeTodoOnDb({
       title: $scope.todoText,
       completed: false
-    };
-    $q.when(db.post(todo))
+    });
+  };
+
+  $scope.writeTodoOnDb = function(todo) {
+    var promiseToCall = todo._id ? db.put(todo) : db.post(todo);
+    $q.when(promiseToCall)
       .then(function() {
+        $scope.todoText = '';
         $scope.showTodos();
-      });
+      }, console.log);
   };
 
   $scope.showTodos = function() {
@@ -23,6 +28,11 @@ app.controller('AppController', function($scope, $q) {
       .then(function(results) {
         $scope.todos = results.rows;
       });
+  };
+
+  $scope.editTodo = function(todo) {
+    todo.title += '.';
+    $scope.writeTodoOnDb(todo);
   };
 
   $scope.removeTodo = function(todo) {
